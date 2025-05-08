d0510a6a6e484b472400c6e55e5aa65cded2dce3
yoryi ojeda <yoryiojeda@outlook.com>
2025-05-08 13:59:27 -0500

    feat: implement FavoriteScreen with header and styling

--- src/screens/favorite-screen.tsx
+++ src/screens/favorite-screen.tsx
@@ -1,18 +1,43 @@
 import React from "react";
 import { Colors } from "../config";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 import { GradientWrapper } from "../components";
 
 const FavoriteScreen = () => {
+  const headerHome = () => {
+    return (
+      <View style={styles.headerContainer}>
+        <Text style={styles.greeting}>Favoritos</Text>
+        <Text style={styles.greeting2}>Aquí puedes ver todos tus coins favoritos</Text>
+      </View>
+    );
+  };
+
   return <GradientWrapper>
-    <View></View>
-  </GradientWrapper> 
+    <View style={{ paddingLeft: 30, paddingRight: 30 }}>
+      {headerHome()}
+    </View>
+  </GradientWrapper>
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.Background,
+  },
+  headerContainer: {
+    gap: 5,
+    marginTop: 35,
+  },
+  greeting2: {
+    fontSize: 15,
+    fontWeight: "bold",
+    color: Colors.TextSecondary,
+  },
+  greeting: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: Colors.Secondary,
   },
 });
 
