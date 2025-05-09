e5a6e67d635344b7b2de8221956397b49718d58b
yoryi ojeda <yoryiojeda@outlook.com>
2025-05-09 02:08:48 -0500

    feat: refactor DetailsScreen header with navigation icons and remove unused text

--- src/screens/details-screen.tsx
+++ src/screens/details-screen.tsx
@@ -1,19 +1,30 @@
 import React from "react";
 import { Colors } from "../config";
 import { Wrapper } from "../components";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 import { RootStackParamList } from "../types/navigation";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { NavigationProp, useNavigation } from "@react-navigation/native";
 
 const DetailsScreen = () => {
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+
   return (
     <Wrapper>
       <View style={{ paddingLeft: 25, paddingRight: 25, height: '100%' }}>
+
+        {/* Header */}
         <View style={styles.headerContainer}>
-          <Text style={styles.greeting}>Favoritos</Text>
-          <Text style={styles.greeting2}>Aquí puedes ver todos tus coins favoritos</Text>
+          <TouchableOpacity onPress={() => navigation.goBack()} style={styles.iconContainer}>
+            <MaterialCommunityIcons name="arrow-left" size={24} color={Colors.secondary} />
+          </TouchableOpacity>
+          <Text style={styles.headerTitle}>USD</Text>
+          <TouchableOpacity onPress={() => console.log("Reload pressed")} style={styles.iconContainer}>
+            <MaterialCommunityIcons name="reload" size={24} color={Colors.secondary} />
+          </TouchableOpacity>
         </View>
+
+
       </View>
     </Wrapper>
   )
@@ -24,19 +35,24 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: Colors.background,
   },
+
   headerContainer: {
-    gap: 5,
-    paddingTop: 20,
+    borderRadius: 20,
+    marginTop: 15,
+    alignItems: "center",
+    flexDirection: "row",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    justifyContent: "space-between",
+    backgroundColor: Colors.background,
   },
-  greeting2: {
-    fontSize: 15,
-    fontWeight: "bold",
-    color: Colors.textPrimary,
-  },
-  greeting: {
+  headerTitle: {
     fontSize: 18,
     fontWeight: "bold",
     color: Colors.secondary,
+  },
+  iconContainer: {
+    padding: 5,
   },
 });
 
