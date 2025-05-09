d5f87a1c5d6be6ae65babf9f44a16a57faa5b984
yoryi ojeda <yoryiojeda@outlook.com>
2025-05-09 01:25:42 -0500

    feat: enhance API service documentation with detailed JSDoc comments

--- src/features/api/index.ts
+++ src/features/api/index.ts
@@ -1,6 +1,19 @@
 import { ApiConfig } from '../../config';
 import ApiService from './services/ApiService';
 
+/**
+ * Configures and initializes the API service with the base URL provided by `ApiConfig`.
+ *
+ * This module creates an instance of `ApiService` using the configured base URL and exports it for use throughout the application.
+ *
+ * @module
+ * @example
+ * ```tsx
+ * import { apiService } from './services/apiService';
+ * apiService.getData(); // Example of how to use the API service
+ * ```
+ */
+
 const apiUrl = ApiConfig.getBaseURL()
 export const apiService = new ApiService(apiUrl);
 export { ApiService };
--- src/features/api/services/ApiService.ts
+++ src/features/api/services/ApiService.ts
@@ -1,7 +1,24 @@
+/**
+ * A service class for making API requests using Axios.
+ *
+ * This class provides methods to make GET and POST requests to the configured API URL.
+ * It initializes an Axios instance with the provided base URL and default headers.
+ *
+ * @class ApiService
+ * @param {string} baseURL - The base URL to be used for all API requests.
+ */
+
 import axios, { AxiosInstance, AxiosResponse } from 'axios';
 
 class ApiService {
     private axiosInstance: AxiosInstance;
+
+    /**
+     * Creates an instance of ApiService with the provided base URL.
+     * 
+     * @param {string} baseURL - The base URL for API requests.
+     */
+
     constructor(baseURL: string) {
         this.axiosInstance = axios.create({
             baseURL,
@@ -10,6 +27,14 @@ class ApiService {
             },
         });
     }
+
+    /**
+     * Makes a GET request to the specified URL and returns the response data.
+     * 
+     * @param {string} url - The API endpoint to request.
+     * @returns {Promise<T>} - The data returned from the GET request.
+     * @throws Will throw an error if the request fails.
+     */
     public async get<T>(url: string): Promise<T> {
         try {
             const response: AxiosResponse<T> = await this.axiosInstance.get<T>(url);
@@ -19,6 +44,15 @@ class ApiService {
             throw error;
         }
     }
+
+    /**
+     * Makes a POST request to the specified URL with the provided data and returns the response data.
+     * 
+     * @param {string} url - The API endpoint to request.
+     * @param {U} data - The data to send in the POST request.
+     * @returns {Promise<T>} - The data returned from the POST request.
+     * @throws Will throw an error if the request fails.
+     */
     public async post<T, U = T>(url: string, data: U): Promise<T> {
         try {
             const response: AxiosResponse<T> = await this.axiosInstance.post<T>(url, data);
